030aedd0a28a5f809d271b988e5b06d85f730918
Nick Vlku <nick.vlku@gmail.com>
2011-12-12 15:17:50 -0800

    Now works with OAuth2 API endpoints.  Included new sample code

--- trove.php
+++ trove.php
@@ -1,153 +1,86 @@
 <?php
 /**
  * The MIT License (MIT)
-
+ *
  * Copyright (c) 2011 Aaron hedges
-
+ * Copyright (c) 2011 Nick Vlku
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+ *
  */
 
 /**
  * 
  * @author Aaron Hedges <aaron@dashron.com>
+ * @author Nick Vlku <n@yourtrove.com>
  * @version 
  */
 class Trove {
 	
-	private $consumerKey;
-	private $consumerSecret;
-	private $token;
-	private $tokenSecret;
-	private $sigMethod = 'HMAC-SHA1';
+    public $clientId;
+	public $clientSecret;
+	public $redirectUri;
+	public $scope;
+	public $accessToken;
 	
 	private static $rootUrl = 'https://api.yourtrove.com/v2/';
-	private static $requestUrl = '/v2/oauth/request_token/';
-	private static $authorizeUrl = '/v2/oauth/authorize/';
-	const VERSION = '1.0';
+    private static $authenticateUrl = 'https://www.yourtrove.com/oauth2/authenticate/';
+	private static $authorizeUrl = 'https://www.yourtrove.com/oauth2/access_token';
+	const VERSION = '2.0';
 	
-	/**
-	 * Getter for the token
-	 * @return string
-	 */
-	public function token() {
-		return $this->token;
-	}
-	
-	/**
-	 * Getter for the token secret
-	 * @return string
-	 */
-	public function tokenSecret() {
-		return $this->tokenSecret;
-	}
 	
 	/**
 	 * Consumer Key and Consumer Secret are required here. These are your oauth credentials
 	 * 
-	 * @param string $consumerKey Required, contains the oauth consumer key
-	 * @param string $consumerSecret Required, contains the oauth consumer secret
-	 * @param string $token Optional, if you have already retrieved a token you can provide it here
-	 * @param string $tokenSecret Optional, if you have already retrieved a token secret you can provide it here
+	 * @param string $clientId Required, contains the oauth consumer key
+	 * @param string $clientSecret Required, contains the oauth consumer secret
+	 * @param string $redirectUri Required, contains the redirectURI for your app specified in YourTrove
+	 * @param string $scope Optional, a list of content types you want.  Default is array('photos')
+     * @param string $accessToken Optional, if you already have this authenticated     
 	 */
-	public function __construct($consumerKey, $consumerSecret, $token = null, $tokenSecret = null) {
-		$this->consumerKey = $consumerKey;
-		$this->consumerSecret = $consumerSecret;
-		$this->token = $token;
-		$this->tokenSecret = $tokenSecret;
+	public function __construct($clientId, $clientSecret, $redirectUri, $accessToken = null,$scope = array('photos')) {
+		$this->clientId = $clientId;
+		$this->clientSecret = $clientSecret;
+		$this->redirectUri = $redirectUri;
+		$this->scope = $scope;
+		$this->accessToken = $accessToken;
 	}
 	
 	/**
-	 * First step of oauth, contact the service provider and get an unauthorized request token.
-	 * No redirections are handled here, the result is the final url string.
-	 * 
-	 * Once called, you MUST pull out the token secret using $trove->tokenSecret(). It will be set
-	 * with an unauthorized token secret, and is required for the next step: acceptToken
+	 * First step of oauth2.  Gets an authenticate request that will bounce back a code that you will use
+	 * to create an Access Token
 	 * 
 	 * @return string Url that the user should be redirected to.
 	 */
 	public function buildAuthURL() {
-		//I believe callback is set in admin for trove
-		//Build post request with any additional parameters
-		//if(!isset($requestParams['oauth_callback'])) {
-		//	 $requestParams['oauth_callback'] = $this->callback;
-		//}
-		
-		//Build the signed request
-		$params = $this->buildRequest('POST', self::$requestUrl); 
+	    $params['client_id'] = $this->clientId;
+	    $params['response_type'] = 'code';
+	    $params['redirect_uri'] = $this->redirectUri;
+	    
+	    
+	    return HttpUtil::createGetUrl(self::$authenticateUrl, $params);   
+	}
+	
+	public function getAccessToken($codeToken) {
+		$params['client_id'] = $this->clientId;
+	    $params['grant_type'] = 'authorization_code';
+	    $params['redirect_uri'] = $this->redirectUri;
+	    $params['client_secret'] = $this->clientSecret;
+	    $params['code'] = $codeToken;
+	    
+	    $response = HttpUtil::httpRequest('GET', self::$authorizeUrl, $params);
+	    $data = json_decode($response);
 
-		//Perform the signed request to retrieve an unauthorized token
-		$data = HttpUtil::httpRequest('POST', self::$requestUrl, $params);
-		
-		//The standard response is a query string, parse it into an array
-		$data = self::parseQuery($data);
-		
-		//Retrieve the oauth_token, this is used in the final authorization url
-		$this->token = $data['oauth_token'];
-		
-		//From the array we pull the token secret. This is used in authorizing a token (step 2)
-		$this->tokenSecret = $data['oauth_token_secret'];
-		
-		//The final url is returned here, containing the configured authroizeTokenUrl and the newly
-		//generated authorization token.
-		return self::$authorizeUrl . "?oauth_token={$requestToken}";
-		
+		$this->accessToken = $data->{'access_token'};
+	    
 	}
 	
-	/**
-	 * This is step two, it must be called from the auth callback page.
-	 * it must be provided with the token secret you received while building the auth url
-	 * 
-	 * @param string tokenSecret the secret provided upon building the authurl
-	 * @param array $params
-	 * @return boolean true if the token and token secret has been located, false otherwise
-	 */
-	public function acceptToken($tokenSecret, $params = array()) {
-		
-		//First we retrieve all the data we can to build the request
-		//From the GET variable we pull the verifier string (for oauth 1.0a) and the token
-		//From the session we pull the token secret, retrieved in step one.
-		
-		//verifier was added in oauth 1.0a
-		if(isset($_GET['oauth_verifier']) && !isset($params['oauth_verifier'])) {
-			$params['oauth_verifier'] = $_GET['oauth_verifier'];
-		}
-		
-		$this->token = $_GET['oauth_token'];
-		$this->tokenSecret = $tokenSecret;
-		/*if(isset($_GET['oauth_token_secret'])) $this->tokenSecret($_GET['oauth_token_secret']);
-		  else {
-			$this->tokenSecret = $tokenSecret;
-		  }
-		*/	
-		 
-		//build the request from the provided parameters
-		$params = $this->buildRequest('POST', $this->accessTokenUrl, $params);
-		
-		//request an access token using the built request
-		$data = CurlUtil::post($this->accessTokenUrl, $params, array(CURLOPT_SSL_VERIFYPEER=>false));
-		
-		//the response is similar to the query section of a url, this parses it into an array
-		$data = CurlUtil::parseQuery($data);
-		
-		//assign the token information to the currently logged in user
-		$this->token = $data['oauth_token'];
-		$this->tokenSecret = $data['oauth_token_secret'];;
-		
-		return !empty($this->token) && !empty($this->tokenSecret);
-	}
 	
-	/**
-	 * performs an http post request along with the necessary oauth information, and signature
-	 * 
-	 * @param string $url
-	 * @param array $params
-	 * @return the body of the http response
-	 */
+	
 	function post($url, $params = array()) {
 		$url = self::$rootUrl . $url;
 		$params = $this->buildRequest("POST", $url, $params);
@@ -158,64 +91,13 @@ class Trove {
 	 * performs an http get request along with the necessary oauth information, and signature
 	 *  
 	 * @param string $url
-	 * @param arrray $params
+	 * @param arrray $param
 	 * @return the body of the http response
 	 */
 	function get($url, $params = array()) {
 		$url = self::$rootUrl . $url;
 		$params = $this->buildRequest("GET", $url, $params);
 		return HttpUtil::httpRequest("GET", $url, $params);
-	}
-	
-	/**
-	 * Builds the oauth request parameters and adds the signature.
-	 *
-	 * @param string $method HTTP method, GET, POST, DELETE etc.
-	 * @param string $url destination url
-	 * @param array $params any additional parameters that are needed
-	 * @return array $params with the oauth data injected
-	 */
-	protected function buildRequest($method, $url, $params = array()) {
-		
-		$params['oauth_consumer_key'] = $this->consumerKey;
-		if(isset($this->token)) {
-			$params['oauth_token'] = $this->token;
-		}
-
-		//cheap and easy nonce
-		//TODO: investigate more whether this is safe
-		//TODO: seed mt_rand?
-		$params['oauth_nonce'] = sha1(mt_rand() . microtime());
-		$params['oauth_timestamp'] = time();
-		$params['oauth_signature_method'] = $this->sigMethod;
-		$params['oauth_version'] = self::VERSION;
-		//$params['realm'] = 'photos';
-		$params['oauth_signature'] = $this->buildSignature($method, $url, $params);
-		return $params;
-	}
-	
-	/**
-	 * Builds the signature hash
-	 *
-	 * @param string $type http request type
-	 * @param string $url http request url
-	 * @param array $params http request parameters
-	 * @return the signature for the provided request type, url and params
-	 */
-	protected function buildSignature($type, $url, $params) {
-		ksort($params);
-		$query = http_build_query($params);
-
-		//build un-hashed signature
-		$signature = self::clean($type) . "&" . self::clean($url) . "&" . $this->clean($query); //eg. GET&http%3A%2F%2Fphotos.example.net%2F
-
-		if($params['oauth_signature_method'] == 'HMAC-SHA1') {
-			$hmac_key = self::clean($this->consumerSecret) . "&";
-			if(isset($this->tokenSecret)) $hmac_key .= self::clean($this->tokenSecret);
-			
-			return base64_encode(hash_hmac('sha1', $signature, $hmac_key, true));			
-		}
-		else throw new Exception('Currently unsupported signature method');
 	}
 	
 	/**
@@ -231,7 +113,50 @@ class Trove {
 		//TODO: Do this the right way?
 		return str_replace('+',' ', str_replace('%7E', '~', $data));
 	}
+	
+	public function getUserInformation()
+	{
+	    if ($this->accessToken == null) {
+	        throw new AccessTokenRequiredException();
+	    }
+	    $params['access_token'] = $this->accessToken;
+	    $response = HttpUtil::httpRequest('GET', self::$rootUrl."/user/", $params);
+	    $user = json_decode($response, true);
+	    return $user;
+	}
+	
+	private function getContent($type, $query) {
+	    if ($this->accessToken == null) {
+	        throw new AccessTokenRequiredException();
+	    }
+	    $params['access_token'] = $this->accessToken;
+	    if ($query != null) {
+	        $params = array_merge($params, $query);
+	    }
+	    print_r($params);
+	    $response = HttpUtil::httpRequest('GET', self::$rootUrl."/content/". $type . "/", $params);
+	    $results = json_decode($response, true);
+	    return $results;
+	    
+	}
+	public function getPhotos($query = null)
+	{
+        return $this->getContent("photos", $query);
+    }
+	public function getCheckins($query = null)
+	{
+        return $this->getContent("checkins", $query);
+    }
+	public function getStatus($query = null)
+	{
+        return $this->getContent("status", $query);
+    }
+
+    
 }
+
+class AccessTokenRequiredException extends Exception {}
+
 
 /**
  * A simple utility to make curl get/post easier, and to decouple the code from curl if 
@@ -240,6 +165,17 @@ class Trove {
  * @author Aaron Hedges <aaron@dashron.com>
  */
 class HttpUtil {
+    public static function createGetUrl($url, $params) {
+        if(isset($params)) {
+			if(strstr($url, '?')) {
+				$url .= '&' . http_build_query($params);
+			}
+			else {
+				$url .= '?' . http_build_query($params);
+			}
+		}
+		return $url;
+    }
 	/**
 	 * Posts the parameters to the provided url
 	 * 
@@ -249,11 +185,8 @@ class HttpUtil {
 	 * @return string the body of the http response.
 	 */
 	public static function httpRequest($method, $url, $params) {
-		var_dump($method, $url, $params);
 		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		
+        
 		if($method == "GET") {
 			if(isset($params)) {
 				if(strstr($url, '?')) {
@@ -269,20 +202,20 @@ class HttpUtil {
 			curl_setopt($curl, CURLOPT_POST, 1);
 			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 		}
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 		
-		//TODO: Remove this, unsafe
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-		
+        
 		$data = curl_exec($curl);
+		
 		if(curl_errno($curl)) {
 			$err = curl_error($curl);
 			curl_close($curl);
 			throw new Exception($err);
 		}
 		$page = curl_getinfo($curl);
-
 		if($page['http_code']!=200) {	 
-			throw new CurlException($page, $data);
+			echo($data);
 		}
 		curl_close($curl);
 		
